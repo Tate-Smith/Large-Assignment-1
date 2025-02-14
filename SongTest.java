0654d7b0fe071a9d79a30493ef2ca0f8a26b8b60
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-02-14 16:27:48 -0700

    Add files via upload

---
+++ SongTest.java
@@ -0,0 +1,47 @@
+package Model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SongTest {
+	
+	@Test
+	void testName() {
+		Song s = new Song("Self Control", "Frank Ocean");
+		Assertions.assertEquals(s.getName(), "Self Control");
+	}
+	
+	@Test
+	void testArtist() {
+		Song s = new Song("Self Control", "Frank Ocean");
+		Assertions.assertEquals(s.getArtist(), "Frank Ocean");
+	}
+	
+	@Test
+	void testToSTring() {
+		Song s = new Song("Self Control", "Frank Ocean");
+		Assertions.assertEquals(s.toString(), "Self Control, by: Frank Ocean");
+	}
+
+	@Test
+	void testFavorite() {
+		Song s = new Song("Self Control", "Frank Ocean");
+		s.favorite();
+		Assertions.assertTrue(s.isFavorite());
+	}
+	
+	@Test
+	void testRate() {
+		Song s = new Song("Self Control", "Frank Ocean");
+		s.rate(2);
+		Assertions.assertEquals(2, s.getRating());
+		Assertions.assertFalse(s.isFavorite());
+	}
+	
+	@Test
+	void testRateTwo() {
+		Song s = new Song("Self Control", "Frank Ocean");
+		s.rate(5);
+		Assertions.assertTrue(s.isFavorite());
+	}
+}
