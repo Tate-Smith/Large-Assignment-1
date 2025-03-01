b0681a73a2170bc96eb0da7e5377f3d9a40c1550
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-02-28 18:20:52 -0700

    updated song to have an album field

--- MusicStoreTest.java
+++ MusicStoreTest.java
@@ -1,15 +1,46 @@
 package Model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MusicStoreTest {
+	private MusicStore store = new MusicStore();
 
 	@Test
 	void testMusicStoreConstructor() {
-		MusicStore store = new MusicStore();
-		assertEquals(store.getAllAlbums().size(), 15);
+		Assertions.assertEquals(store.getAllAlbums().size(), 15);
+	}
+	
+	@Test 
+	void testGetSong() {
+		Song s = store.getSong("Jesus", "Amos Lee");
+		Assertions.assertEquals(s.toString(), "Mission Bell, by: The Heavy");
+	}
+	
+	@Test
+	void testGetAlbum() {
+		Album a = store.getAlbum("Mission Bell", "Amos Lee");
+		Assertions.assertEquals(a.getArtist(), "Amos Lee");
+	}
+	
+	@Test
+	void testGetSongTitle() {
+		
+	}
+	
+	@Test
+	void testGetSongArtist() {
+		
+	}
+	
+	@Test
+	void testGetAlbumTitle() {
+		
+	}
+	
+	@Test
+	void testGetAlbumArtist() {
+		
 	}
 
 }
